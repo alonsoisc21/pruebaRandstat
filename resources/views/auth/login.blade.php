c67f73729b132f1931253ae9f1849e2c062a1e03
Alonso Chavarria <alonso.chavarria@globalcorporation.cc>
2020-05-21 14:37:05 -0500

    Inicio de Sesion

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
-@section('titulo','Login')
+@section('titulo','Inicio de Sesión')
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">{{ __('Inicio de Sesión') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
